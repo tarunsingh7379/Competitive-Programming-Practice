2cbe677986b2ea5f03903adba8563a4113521025
Tarun Singh <tarunsinghofficial236@gmail.com>
2023-02-04 10:53:20 +0530

    1600 rating codeforces problem solution added.

---
+++ Codeforces/1600/A_DZY_Loves_Sequences.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n;
+    cin >> n;
+    ll a[n];
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    if (n == 1 || n == 2)
+    {
+        cout << n << endl;
+    }
+    else
+    {
+        vector<ll> right(n);
+        right[n - 1] = 1;
+        for (ll i = n - 2; i >= 0; i--)
+        {
+            if (a[i] < a[i + 1])
+            {
+                right[i] = right[i + 1] + 1;
+            }
+            else
+            {
+                right[i] = 1;
+            }
+        }
+        ll ans = 0, cnt = 1;
+        for (ll i = 0; i < n; i++)
+        {
+            if (i >= 2)
+            {
+                if (a[i - 1] > a[i - 2])
+                {
+                    cnt++;
+                }
+                else
+                {
+                    cnt = 1;
+                }
+            }
+            if (i == 0)
+            {
+                ans = max(ans, right[i + 1] + 1);
+            }
+            else if (i < n - 1)
+            {
+                if (a[i - 1] + 1 < a[i + 1])
+                {
+                    ans = max(ans, cnt + 1 + right[i + 1]);
+                }
+                else
+                {
+                    ans = max(ans, cnt + 1);
+                    ans = max(ans, right[i + 1] + 1);
+                }
+            }
+            else
+            {
+                ans = max(ans, cnt + 1);
+            }
+        }
+        cout << ans << endl;
+    }
+
+    return 0;
+}
