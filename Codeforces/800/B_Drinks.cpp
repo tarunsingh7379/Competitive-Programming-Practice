22f13c21c161e5ea782727a05c6656b607eabbd0
Tarun Singh <tarunsinghofficial236@gmail.com>
2023-02-26 21:40:06 +0530

    800 rating codeforces problem solution added.

---
+++ Codeforces/800/B_Drinks.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        sum += x;
+    }
+    cout << fixed << setprecision(12) << 1.0 * sum / n << endl;
+    
+    return 0;
+}
