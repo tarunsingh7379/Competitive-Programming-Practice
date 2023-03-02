781713b803aa161185bedb8e95fff9b79fa9eef5
Tarun Singh <tarunsinghofficial236@gmail.com>
2023-03-02 08:46:10 +0530

    800 rating codeforces problem solution added.

---
+++ Codeforces/800/A_Pangram.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    transform(s.begin(), s.end(), s.begin(), ::tolower);
+
+    set<char> st(s.begin(), s.end());
+    if (st.size() == 26)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
