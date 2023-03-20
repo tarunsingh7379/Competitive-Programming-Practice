1be5623f2c8a117074b7938d70e2397a627765a4
Tarun Singh <tarunsinghofficial236@gmail.com>
2023-03-20 08:52:23 +0530

    800 rating codeforces problem solution added.

---
+++ Codeforces/800/A_Again_Twenty_Five.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    int p = 1;
+
+    if (n == 1)
+    {
+        cout << 5 << endl;
+    }
+    else
+    {
+        cout << 25 << endl;
+    }
+
+    return 0;
+}
