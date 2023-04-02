b6e9fec948a9bd6f7071bba2d75344355b18e759
Tarun Singh <tarunsinghofficial236@gmail.com>
2023-04-02 09:33:05 +0530

    800 rating codeforces problem solution added.

---
+++ Codeforces/800/A_EhAb_AnD_gCd.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t)
+    {
+        int x;
+        cin >> x;
+        cout << 1 << " " << (x - 1) << endl;
+        t--;
+    }
+
+    return 0;
+}
