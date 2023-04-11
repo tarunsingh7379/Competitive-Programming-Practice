dff6799e2851af4b06cf3de5d1205718885bf344
Tarun Singh <tarunsinghofficial236@gmail.com>
2023-04-11 23:48:28 +0530

    800 rating codeforces problem solution added.

---
+++ Codeforces/800/A_Coins.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t)
+    {
+        ll n, k;
+        cin >> n >> k;
+        if ((n - k) % 2 == 0 || n % 2 == 0 || n % k == 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+        t--;
+    }
+
+    return 0;
+}
