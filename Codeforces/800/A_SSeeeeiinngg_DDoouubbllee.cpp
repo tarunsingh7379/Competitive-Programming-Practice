bed484d57b396e51c654380db3ceb2a0dd4be389
Tarun Singh <tarunsinghofficial236@gmail.com>
2023-05-08 18:38:33 +0530

    800 rating codeforces problem solution added.

---
+++ Codeforces/800/A_SSeeeeiinngg_DDoouubbllee.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t)
+    {
+        string s;
+        cin >> s;
+        cout << s;
+        reverse(s.begin(), s.end());
+        cout << s << endl;
+        t--;
+    }
+
+    return 0;
+}
