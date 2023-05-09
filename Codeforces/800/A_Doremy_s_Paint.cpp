2496af8015ff5ad0887b4835160a61c240289fdb
Tarun Singh <tarunsinghofficial236@gmail.com>
2023-05-09 09:19:49 +0530

    800 rating codeforces problem solution added.

---
+++ Codeforces/800/A_Doremy_s_Paint.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t)
+    {
+        int n;
+        cin >> n;
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+        }
+        cout << 1 << " " << n << endl;
+        t--;
+    }
+
+    return 0;
+}
