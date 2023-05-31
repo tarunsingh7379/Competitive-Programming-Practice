7f1076036b568008eb824180df42a603047ee79d
Tarun Singh <tarunsinghofficial236@gmail.com>
2023-05-31 09:48:38 +0530

    900 rating codeforces problem solution added.

---
+++ Codeforces/900/A_Odd_Divisor.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t)
+    {
+        ll n;
+        cin >> n;
+        while (n > 0 && n % 2 == 0)
+        {
+            n /= 2;
+        }
+        if (n != 1)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+        t--;
+    }
+
+    return 0;
+}
