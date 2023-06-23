7a16ea69a5f2f8e07fb0fb537e36ca52a770d291
Tarun Singh <tarunsinghofficial236@gmail.com>
2023-06-23 20:56:26 +0530

    1000 rating codeforces problem solution added.

---
+++ Codeforces/1000/C_Sum_in_Binary_Tree.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t)
+    {
+        ll n;
+        cin >> n;
+        ll ans = 0;
+        while (n)
+        {
+            ans += n;
+            n /= 2;
+        }
+        cout << ans << endl;
+        t--;
+    }
+
+    return 0;
+}
