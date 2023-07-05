b98548714f74bad74c26013bf076f77ced4d2f5d
Tarun Singh <tarunsinghofficial236@gmail.com>
2023-07-05 22:12:50 +0530

    900 rating codeforces problem solution added.

--- Codeforces/900/A_Puzzles.cpp
+++ Codeforces/900/A_Puzzles.cpp
@@ -20,11 +20,11 @@ int main()
     sort(a, a + m);
 
     int ans = INT_MAX;
-    for (int i = 0; i + n - 1 <= m; i++)
+    for (int i = 0; i + n - 1 < m; i++)
     {
         ans = min(ans, a[i + n - 1] - a[i]);
     }
     cout << ans << endl;
-    
+
     return 0;
 }
