a3a030d063a45b06e404d6a62d439a34be65ef2c
Tarun Singh <tarunsinghofficial236@gmail.com>
2023-10-05 21:15:24 +0530

    Add 1000 rating codeforces problem solution

---
+++ Codeforces/1000/A_Fake_NP.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int l, r;
+    cin >> l >> r;
+    cout << (l == r ? l : 2) << endl;
+
+    return 0;
+}
