aafbebe9d0df3673520b1135966ab1709b87fefc
Tarun Singh <tarunsinghofficial236@gmail.com>
2023-10-20 20:46:39 +0530

    Add 1000 rating codeforces problem solution

---
+++ Codeforces/1000/A_Find_Amir.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    cout << (n - 1) / 2 << endl;
+
+    return 0;
+}
