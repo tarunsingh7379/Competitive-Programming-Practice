161d43f55f7be8fe6f7ea9dc84016915627c21b2
Tarun Singh <tarunsinghofficial236@gmail.com>
2023-11-15 20:32:36 +0530

    Add 1000 rating codeforces problem solution

---
+++ Codeforces/1000/A_Fashion_in_Berland.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    int a[n];
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        sum += a[i];
+    }
+    if (n == 1)
+    {
+        if (sum == 1)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    else
+    {
+        if (sum == n - 1)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
