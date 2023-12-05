3686d5279b7eaea7f928a93a0974dd6d6b892b9a
Tarun Singh <tarunsinghofficial236@gmail.com>
2023-12-05 16:30:04 +0530

    Add 1200 rating codeforces problem solution

---
+++ Codeforces/1200/A_Next_Test.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    int a[n];
+    map<int, int> mp;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        mp[a[i]]++;
+    }
+    int ans = 1;
+    while (mp[ans])
+    {
+        ans++;
+    }
+    cout << ans << endl;
+    return 0;
+}
