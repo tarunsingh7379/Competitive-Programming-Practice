0be300b85d74c836e9d648660804940b14724ea1
Tarun Singh <tarunsinghofficial236@gmail.com>
2023-12-13 20:54:22 +0530

    Add 1200 rating codeforces problem solution

---
+++ Codeforces/1200/B_Paranoid_String.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t)
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        ll ans = 0;
+        for (ll i = 1; i < n; i++)
+        {
+            if (s[i] != s[i - 1])
+            {
+                ans += i;
+            }
+        }
+        ans += n;
+        cout << ans << endl;
+        t--;
+    }
+
+    return 0;
+}
