2dbc9e6225d152b6fae263ea9c6f61d7e0025303
Tarun Singh <tarunsinghofficial236@gmail.com>
2024-02-12 10:25:38 +0530

    Add 1200 rating codeforces problem solution

---
+++ Codeforces/1200/B_Unnatural_Conditions.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n, m;
+    cin >> n >> m;
+    cout << string(200, '9') << string(199, '0') << 1 << endl;
+    cout << string(200, '9') << endl;
+
+    return 0;
+}
