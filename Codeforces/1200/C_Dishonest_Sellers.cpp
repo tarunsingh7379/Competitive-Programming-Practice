6b77e15a8d97d4d360e90a5cd58f28a2cc39df6e
Tarun Singh <tarunsinghofficial236@gmail.com>
2024-04-29 12:15:27 +0530

    Add 1200 rating codeforces problem solution

--- Codeforces/1200/C_Dishonest_Sellers.cpp
+++ Codeforces/1200/C_Dishonest_Sellers.cpp
@@ -5,49 +5,46 @@ using namespace std;
 
 bool cmp(pair<ll, ll> &a, pair<ll, ll> &b)
 {
-    ll d1 = a.first - a.second;
-    ll d2 = b.first - b.second;
-    return d1 <= d2;
+    ll d1 = a.second - a.first;
+    ll d2 = b.second - b.first;
+    if (d1 > d2)
+    {
+        return true;
+    }
+    return false;
 }
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
     ll n, k;
     cin >> n >> k;
-    ll a[n];
+    ll a[n], b[n];
     for (ll i = 0; i < n; i++)
     {
         cin >> a[i];
     }
-    ll b[n];
     for (ll i = 0; i < n; i++)
     {
         cin >> b[i];
     }
-
     vector<pair<ll, ll>> v;
     for (ll i = 0; i < n; i++)
     {
         v.push_back({a[i], b[i]});
     }
-
     sort(v.begin(), v.end(), cmp);
-    ll l = 0;
     ll ans = 0;
-    while (l < n && k > 0)
+    for (ll i = 0; i < k; i++)
     {
-        ans += v[l].first;
-        l++;
-        k--;
+        ans += v[i].first;
     }
-    while (l < n)
+    for (ll i = k; i < n; i++)
     {
-        ans += min(v[l].first, v[l].second);
-        l++;
+        ans += min(v[i].first, v[i].second);
     }
     cout << ans << endl;
+
     return 0;
 }
