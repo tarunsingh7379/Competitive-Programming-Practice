70d4860f2a35aee49953e796303ab97bf8cde532
Tarun Singh <tarunsinghofficial236@gmail.com>
2024-05-22 18:56:18 +0530

    Add 1300 rating codeforces problem solution

---
+++ Codeforces/1300/B_Pashmak_and_Flowers.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    int a[n];
+    map<int, int> mp;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        mp[a[i]]++;
+    }
+    sort(a, a + n);
+    if (a[0] == a[n - 1])
+    {
+        cout << 0 << " " << n * (n - 1) / 2 << endl;
+    }
+    else
+    {
+        cout << a[n - 1] - a[0] << " " << mp[a[0]] * mp[a[n - 1]] << endl;
+    }
+
+    return 0;
+}
