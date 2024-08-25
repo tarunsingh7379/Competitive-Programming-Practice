beba5ac85efd76cadf9fb6940881bc768e9f614d
Tarun Singh <tarunsinghofficial236@gmail.com>
2024-08-25 10:50:40 +0530

    Add 1300 rating codeforces problem solution

---
+++ Codeforces/1300/A_Oh_Those_Palindromes.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    sort(s.begin(), s.end());
+    cout << s << endl;
+
+    return 0;
+}
