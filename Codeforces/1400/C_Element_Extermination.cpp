7ddee94c0357eebcb130e1531adb33d455aa3997
Tarun Singh <tarunsinghofficial236@gmail.com>
2024-11-18 12:45:46 +0530

    Add 1400 rating codeforces problem solution

---
+++ Codeforces/1400/C_Element_Extermination.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        if (a[0] < a[n - 1])
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+        t--;
+    }
+
+    return 0;
+}
