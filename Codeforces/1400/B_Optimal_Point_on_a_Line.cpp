985f5727b71ec969a09636cefba47432d3599796
Tarun Singh <tarunsinghofficial236@gmail.com>
2025-01-31 21:33:06 +0530

    Add 1400 rating codeforces problem solution

---
+++ Codeforces/1400/B_Optimal_Point_on_a_Line.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n;
+    cin >> n;
+    ll a[n];
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a, a + n);
+    cout << a[(n - 1) / 2] << endl;
+
+    return 0;
+}
