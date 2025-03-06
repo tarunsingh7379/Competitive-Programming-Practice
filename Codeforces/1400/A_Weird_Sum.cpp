186fefc5e0544db7ee53c44207943d3caf625e5f
Tarun Singh <tarunsinghofficial236@gmail.com>
2025-03-06 19:48:07 +0530

    Add 1400 rating codeforces problem solution

--- Codeforces/1400/A_Weird_Sum.cpp
+++ Codeforces/1400/A_Weird_Sum.cpp
@@ -11,13 +11,39 @@ int main()
     ll n, m;
     cin >> n >> m;
     ll a[n][m];
+    map<ll, vector<ll>> rows;
+    map<ll, vector<ll>> cols;
+    set<ll> colors;
     for (ll i = 0; i < n; i++)
     {
         for (ll j = 0; j < m; j++)
         {
             cin >> a[i][j];
+            rows[a[i][j]].push_back(i + 1);
+            cols[a[i][j]].push_back(j + 1);
+            colors.insert(a[i][j]);
         }
     }
+    ll ans = 0;
+    for (auto color : colors)
+    {
+        ll idx = 0, k = rows[color].size();
+        sort(rows[color].begin(), rows[color].end());
+        sort(cols[color].begin(), cols[color].end());
+        for (auto num : rows[color])
+        {
+            ans = ans + (2 * idx + 1 - k) * num;
+            idx++;
+        }
+        idx = 0;
+        k = cols[color].size();
+        for (auto num : cols[color])
+        {
+            ans = ans + (2 * idx + 1 - k) * num;
+            idx++;
+        }
+    }
+    cout << ans << endl;
 
     return 0;
 }
