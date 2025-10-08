6a457a935d838766f7537a7f1c71ce374a833260
Tarun Singh <tarunsinghofficial236@gmail.com>
2025-10-08 19:02:45 +0530

    Add 1500 rating codeforces problem solution

--- Codeforces/1500/B_Queue.cpp
+++ Codeforces/1500/B_Queue.cpp
@@ -10,26 +10,36 @@ int main()
 
     int n;
     cin >> n;
-    int a[n];
+    map<int, int> mp1, mp2;
+    map<int, int> freq;
     for (int i = 0; i < n; i++)
     {
-        cin >> a[i];
+        int x, y;
+        cin >> x >> y;
+        mp1[x] = y;
+        mp2[y] = x;
+        freq[x]++;
+        freq[y]++;
     }
-    vector<int> suffix(n, INT_MAX);
-    suffix[n - 1] = a[n - 1];
-    for (int i = n - 2; i >= 0; i--)
+    vector<int> ans(n, 0);
+    for (auto num : freq)
     {
-        suffix[i] = min(a[i], suffix[i + 1]);
-    }
-    vector<int> ans(n, -1);
-    for (int i = 0; i < n; i++)
-    {
-        int ind = lower_bound(suffix.begin() + i, suffix.end(), a[i]) - suffix.begin();
-        ind--;
-        if (ind >= 0 && ind > i)
+        if (num.second == 1)
         {
-            ans[i] = ind - i - 1;
+            if (mp1.find(num.first) != mp1.end())
+            {
+                ans[0] = num.first;
+            }
+            else
+            {
+                ans[n - 1] = num.first;
+            }
         }
+    }
+    ans[1] = mp1[0];
+    for (int i = 2; i < n - 1; i++)
+    {
+        ans[i] = mp1[ans[i - 2]];
     }
     for (auto num : ans)
     {
