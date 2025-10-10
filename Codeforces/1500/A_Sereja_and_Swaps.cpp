76ea70030fd640e7d204272128b5e9fd50ce08af
Tarun Singh <tarunsinghofficial236@gmail.com>
2025-10-10 11:30:02 +0530

    Add 1500 rating codeforces problem solution

--- Codeforces/1500/A_Sereja_and_Swaps.cpp
+++ Codeforces/1500/A_Sereja_and_Swaps.cpp
@@ -7,13 +7,51 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int t;
-    cin >> t;
-    while (t)
-    {
 
-        t--;
+    ll n, kk;
+    cin >> n >> kk;
+    ll a[n];
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
     }
+    ll ans = -1e17;
+    for (ll i = 0; i < n; i++)
+    {
+        for (ll j = i; j < n; j++)
+        {
+            vector<ll> v1, v2;
+            for (ll k = 0; k < n; k++)
+            {
+                if (k >= i && k <= j)
+                    v1.push_back(a[k]);
+                else
+                    v2.push_back(a[k]);
+            }
+            sort(v1.begin(), v1.end());
+            ll sum = 0;
+            for (auto num : v1)
+            {
+                sum += num;
+            }
+            ans = max(ans, sum);
+            if (v2.size() > 0)
+            {
+                sort(v2.begin(), v2.end());
+                ll ind1 = 0, ind2 = v2.size() - 1;
+                for (ll op = 0; op < kk; op++)
+                {
+                    if (ind1 < v1.size() && ind2 >= 0)
+                    {
+                        sum -= v1[ind1++];
+                        sum += v2[ind2--];
+                    }
+                    ans = max(ans, sum);
+                }
+            }
+        }
+    }
+    cout << ans << endl;
 
     return 0;
 }
