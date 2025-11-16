eb069137b7c9972189f5672f970058c107992901
Tarun Singh <tarunsinghofficial236@gmail.com>
2025-11-16 10:52:58 +0530

    Add 1500 rating codeforces problem solution

--- Codeforces/1500/B_Factorial_Divisibility.cpp
+++ Codeforces/1500/B_Factorial_Divisibility.cpp
@@ -11,7 +11,7 @@ int main()
     int n, k;
     cin >> n >> k;
     int a[n];
-    vector<int> freq(k, 0);
+    vector<int> freq(k + 1, 0);
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
