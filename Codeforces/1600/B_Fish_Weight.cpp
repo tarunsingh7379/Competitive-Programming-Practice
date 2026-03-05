74bdc02a306e96fd3937710679ba38f536905bda
Tarun Singh <tarunsinghofficial236@gmail.com>
2026-03-05 19:18:23 +0530

    Add 1600 rating codeforces problem solution

---
+++ Codeforces/1600/B_Fish_Weight.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, m, k;
+    cin >> n >> m >> k;
+    int a[n], b[m];
+    map<int, int> mp;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        mp[a[i]]++;
+    }
+    for (int i = 0; i < m; i++)
+    {
+        cin >> b[i];
+        mp[b[i]]--;
+    }
+    for (auto it = mp.rbegin(); it != mp.rend(); it++)
+    {
+        if (it->second > 0)
+        {
+            cout << "YES" << endl;
+            return 0;
+        }
+    }
+    cout << "NO" << endl;
+    return 0;
+}
