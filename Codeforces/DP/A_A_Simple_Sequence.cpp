e19de31c11ece632c169887fd5ea8587c7c54c4b
Tarun Singh <tarunsinghofficial236@gmail.com>
2026-05-01 11:13:37 +0530

    Add 800 rating codeforces problem solution

---
+++ Codeforces/DP/A_A_Simple_Sequence.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t)
+    {
+        int n;
+        cin >> n;
+        for (int i = n; i >= 1; i--)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+        t--;
+    }
+
+    return 0;
+}
