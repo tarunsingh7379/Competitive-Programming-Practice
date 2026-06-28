ac027ff0cc02e9179e4b1f2b99f2166165195e28
Tarun Singh <tarunsinghofficial236@gmail.com>
2026-06-28 12:30:40 +0530

    Add 1100 rating codeforces problem solution

---
+++ Codeforces/DP/B_Binomial_Coefficients_Kind_Of.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+ll power(ll base, ll n, ll m)
+{
+    ll ans = 1;
+    base = base % m;
+    while (n != 0)
+    {
+        if (n % 2 == 1)
+        {
+            n = n - 1;
+            ans = (ans * base) % m;
+        }
+        else
+        {
+            n = n / 2;
+            base = (base * base) % m;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n;
+    cin >> n;
+    vector<ll> a(n), b(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> b[i];
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        cout << power(2, b[i], M) << endl;
+    }
+
+    return 0;
+}
