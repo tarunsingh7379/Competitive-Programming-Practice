815c9a336dee2d21d0608542a11851400c1d50ca
Tarun Singh <tarunsinghofficial236@gmail.com>
2026-07-10 20:36:27 +0530

    Add 1100 rating codeforces problem solution

---
+++ Codeforces/DP/A_Alex_and_a_Rhombus.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    vector<int> dp(n + 1, 0);
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        dp[i] = dp[i - 1] + 4 * (i - 1);
+    }
+    cout << dp[n] << endl;
+
+    return 0;
+}
