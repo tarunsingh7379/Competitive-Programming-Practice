d2655a579fc1bebe2bce889676cc1b543df3c70b
Tarun Singh <tarunsinghofficial236@gmail.com>
2026-07-19 12:36:20 +0530

    Add 900 rating codeforces problem solution

---
+++ Codeforces/DP/B_Crimson_Triples.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t)
+    {
+        ll n;
+        cin >> n;
+        ll ans = 0;
+        for (ll i = 1; i <= n; i++)
+        {
+            ll x = n / i;
+            ans = ans + (x * x);
+        }
+        cout << ans << endl;
+        t--;
+    }
+
+    return 0;
+}
