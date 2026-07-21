eb577ef125013827de72edb61240236b42e1c4ff
Tarun Singh <tarunsinghofficial236@gmail.com>
2026-07-21 20:11:15 +0530

    Add 800 rating codeforces problem solution

---
+++ Codeforces/DP/A_Creating_Words.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t)
+    {
+        string a, b;
+        cin >> a >> b;
+        swap(a[0], b[0]);
+        cout << a << " " << b << endl;
+        t--;
+    }
+
+    return 0;
+}
