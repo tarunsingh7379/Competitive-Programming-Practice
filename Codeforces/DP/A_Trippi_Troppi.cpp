5abe9cab033b0df3b73929d22d968e3da9cd203b
Tarun Singh <tarunsinghofficial236@gmail.com>
2026-08-04 19:41:10 +0530

    Add 800 rating codeforces problem solution

---
+++ Codeforces/DP/A_Trippi_Troppi.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t)
+    {
+        string a, b, c;
+        cin >> a >> b >> c;
+        cout << a[0] << b[0] << c[0] << endl;
+        t--;
+    }
+
+    return 0;
+}
