a93bc0fbeb9c2e176caaedad4164a10938be07d2
Tarun Singh <tarunsinghofficial236@gmail.com>
2026-08-22 09:52:44 +0530

    Add 800 rating codeforces problem solution

---
+++ Codeforces/DP/A_Stair_Peak_or_Neither.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a < b && b < c)
+        {
+            cout << "STAIR" << endl;
+        }
+        else if (b > a && b > c)
+        {
+            cout << "PEAK" << endl;
+        }
+        else
+        {
+            cout << "NONE" << endl;
+        }
+        t--;
+    }
+
+    return 0;
+}
