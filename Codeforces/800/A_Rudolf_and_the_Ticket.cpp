ccc9a55c331aa5d899c5fdbf332857e81a4d2cd0
Tarun Singh <tarunsinghofficial236@gmail.com>
2026-09-07 18:57:40 +0530

    Add 800 rating codeforces problem solution

---
+++ Codeforces/800/A_Rudolf_and_the_Ticket.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t)
+    {
+        int n, m, k;
+        cin >> n >> m >> k;
+        int a[n], b[m];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        for (int i = 0; i < m; i++)
+        {
+            cin >> b[i];
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                ans += (a[i] + b[j] <= k);
+            }
+        }
+        cout << ans << endl;
+        t--;
+    }
+
+    return 0;
+}
