4a1c1557c3370a8b734d03eaaadc83ce8b770d2c
Tarun Singh <tarunsinghofficial236@gmail.com>
2026-09-08 18:40:06 +0530

    Add 800 rating codeforces problem solution

---
+++ Codeforces/800/B_Good_Kid.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t)
+    {
+        ll n;
+        cin >> n;
+        ll a[n];
+        for (ll i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        ll ans = 0;
+        for (ll i = 0; i < n; i++)
+        {
+            ll p = 1;
+            for (ll j = 0; j < n; j++)
+            {
+                p = p * (a[j] + (i == j));
+            }
+            ans = max(ans, p);
+        }
+        cout << ans << endl;
+        t--;
+    }
+
+    return 0;
+}
