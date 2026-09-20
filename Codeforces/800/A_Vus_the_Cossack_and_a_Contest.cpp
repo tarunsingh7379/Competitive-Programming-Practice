1e69c8eaf5354800cd0905e9fed67d583c42bb51
Tarun Singh <tarunsinghofficial236@gmail.com>
2026-09-20 10:37:37 +0530

    Add 800 rating codeforces problem solution

---
+++ Codeforces/800/A_Vus_the_Cossack_and_a_Contest.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, m, k;
+    cin >> n >> m >> k;
+    cout << (n <= min(m, k) ? "Yes" : "No") << endl;
+
+    return 0;
+}
